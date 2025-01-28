9f1523c9be76f774700b30b613ff8cf726d001a5
Nicolas Paiuca Buscarini <nicolasbuscarini@hotmail.com>
2025-01-28 20:47:09 -0300

    INIT: Add .editorconfig, GitHub Actions, and new CSV library
    Introduced a comprehensive .editorconfig file for C# code formatting, coding conventions, and naming styles. Added a GitHub Actions workflow to automate NuGet package publishing. Created a new .NET project for the CodeFlow.Csv.Package library, including a solution file. Added classes for CSV file validation and enum description conversion using CsvHelper.

---
+++ CodeFlow.Csv.Package/Validator/CsvFileValidator.cs
@@ -0,0 +1,67 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodeFlow.Csv.Package.Validator;
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="classMap"></param>
+public class CsvFileValidator(ClassMap classMap)
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="fileName"></param>
+    /// <returns></returns>
+    public static bool IsCsvFile(string fileName)
+    {
+        return fileName.EndsWith(".csv", StringComparison.OrdinalIgnoreCase);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="fileStream"></param>
+    /// <param name="errors"></param>
+    /// <returns></returns>
+    public bool HasValidHeaders(Stream fileStream, CsvConfiguration csvConfiguration, out List<string> errors)
+    {
+        errors = [];
+
+        using var reader = new StreamReader(fileStream, leaveOpen: true);
+        var csv = new CsvReader(reader, csvConfiguration);
+        csv.Context.RegisterClassMap(classMap);
+        csv.Read();
+        csv.ReadHeader();
+        var headerRecord = csv.HeaderRecord;
+
+        if (headerRecord == null)
+        {
+            errors.Add("The CSV file does not contain headers.");
+            return false;
+        }
+
+        var headerRecordNormalized = headerRecord
+            .SelectMany(header => header.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries))
+            .Select(header => header.Trim().ToLowerInvariant().Replace("\"", ""))
+            .ToList();
+
+        var expectedHeaders = CsvFileValidatorHelpers.GetExpectedHeaders(classMap);
+        var expectedHeadersNormalized = expectedHeaders
+            .Select(header => header.Trim().ToLowerInvariant().Replace("\"", ""))
+            .ToList();
+
+        if (!expectedHeadersNormalized.All(headerRecordNormalized.Contains))
+        {
+            errors.Add("The CSV file headers are incorrect.");
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ CodeFlow.Csv.Package/Validator/CsvFileValidatorHelpers.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper.Configuration;
+using System.Collections.Generic;
+
+namespace CodeFlow.Csv.Package.Validator;
+
+public static class CsvFileValidatorHelpers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="classMap"></param>
+    /// <returns></returns>
+    public static IEnumerable<string> GetExpectedHeaders(ClassMap classMap)
+    {
+        var headers = new List<string>();
+
+        foreach (var memberMap in classMap.MemberMaps)
+        {
+            headers.AddRange(memberMap.Data.Names);
+        }
+
+        foreach (var referenceMap in classMap.ReferenceMaps)
+        {
+            headers.AddRange(GetExpectedHeaders(referenceMap.Data.Mapping));
+        }
+
+        foreach (var parameterMap in classMap.ParameterMaps)
+        {
+            headers.AddRange(parameterMap.Data.Names);
+        }
+
+        return headers;
+    }
+}
